acd745d330d897863e266cdfb7c7e6078fd2d0ad
Marcus Vinicius <marcusvinid3v@gmail.com>
2022-06-25 05:26:00 -0300

    initial setup and app grid setted

---
+++ README.md
@@ -0,0 +1,1 @@
+# Svelte Task App
---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  
+</script>
+
+<header>
+  header
+</header>
+
+<main>
+  main content
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Inter, Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-size: 1rem;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
