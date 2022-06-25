71446cdcb007071c90c52ae45c8df1bf7cbbcddc
Marcus Vinicius <marcusvinid3v@gmail.com>
2022-06-25 08:28:15 -0300

    adding task remotion resource and preventing add a new task if it's empty

--- src/Components/todos.svelte
+++ src/Components/todos.svelte
@@ -7,12 +7,21 @@ import { tasks } from "../store/tasks";
 
 const handleAddTask = (e: SubmitEvent) => {
     e.preventDefault();
+
+    if (!taskTitle) return;
+
     addTask({ id: Math.random(), title: taskTitle, date: new Date() });
     taskTitle = '';
 }
 
 const addTask = (task: ITask) => $tasks = [...($tasks || []), task];
+
+const deleteTask = (id: number) => $tasks = $tasks.filter(task => task.id !== id);
+
 </script>
+<svelte:head>
+    <link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
+</svelte:head>
 
 <div id="box">
     <form id="new-task-box" on:submit={handleAddTask}>
@@ -28,6 +37,9 @@ const addTask = (task: ITask) => $tasks = [...($tasks || []), task];
         {#each $tasks as task}
         <div class="task">
             <h4>{task.title}</h4>
+            <button class="delete-task-button" on:click={() => deleteTask(task.id)}>
+                <i class="mi mi-close"></i>
+            </button>
         </div>
         {/each}
     </div>
@@ -64,5 +76,16 @@ const addTask = (task: ITask) => $tasks = [...($tasks || []), task];
         padding: 10px;
         font-weight: 500;
         margin: 10px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .delete-task-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 0;
+        background-color: transparent;
     }
 </style>
